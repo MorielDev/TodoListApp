04332e647e9ef30774d2e9562203ab14d097efe7
Saintmoriel <nimrodyusuf@gmail.com>
2025-02-28 01:05:27 +0100

    fix: Fixed a bug from the button

--- components/Button.tsx
+++ components/Button.tsx
@@ -11,9 +11,9 @@ export default function FAB({ onPress, className }: FABProps) {
   return (
     <TouchableOpacity
       onPress={onPress}
-      className={`absolute bottom-5 left-1/2 -translate-x-1/2 bg-yellow-400 w-16 h-16 rounded-full flex items-center justify-center shadow-lg ${className}`}
+      className={`absolute bottom-11 left-1/2 -translate-x-1/2 bg-yellow-400 w-16 h-16 rounded-full flex items-center justify-center shadow-lg ${className}`}
     >
-      <Ionicons name="add" size={32} color="black" />
+      <Ionicons name="add" size={36} color="black" />
     </TouchableOpacity>
   );
 }
