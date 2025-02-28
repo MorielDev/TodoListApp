5cc3735b6068edba52f813e8365755f4cb41a9dc
Saintmoriel <nimrodyusuf@gmail.com>
2025-02-28 02:05:53 +0100

    Initial commit

---
+++ app/tabs/placeholder.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Placeholder() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-900">
+    </View>
+  );
+}
